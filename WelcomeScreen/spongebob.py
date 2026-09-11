dbdc7d756147db6d8e7a0d8b8ca068d4d9907598
natashateehan <tiny.pikachu2@gmail.com>
2026-09-11 15:16:01 -0400

    added background color "wheel" to change background color added disclaimer!

---
+++ WelcomeScreen/spongebob.py
@@ -0,0 +1,9 @@
+import random
+toons = ["Spongebob", "Patrick", "squidward", "Sandy", "Mr. Krabs", "Gary"]
+
+for toon in toons:
+    SB = input("which Spongebob charter are you more like?")
+    print("Congratulations you are: ", SB, toon)
+
+input("Would you like to play again Y/N")
+if "Y":
